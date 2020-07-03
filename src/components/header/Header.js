f8b2344bed9b6734562343105243f48534c3eacd
PashaShevchuk <pasha608@gmail.com>
2020-07-03 23:09:29 +0300

    - add styles for movie details <Link>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -14,22 +14,32 @@ const HeaderComponent = (props) => {
     };
     return (
         <div className="header navbar">
-            <img src={logo} className="header-logo" alt="logotype"/>
-            <div className="header-links-wrapper">
-                {
-                    links.map(item => {
-                        return (
-                            <div className="nav-item" key={item.url}>
-                                <Link to={item.url} className="header-links-wrapper-link nav-link">{item.name}</Link>
-                            </div>
-                        );
-                    })
-                }
+            <div className="container">
+
+                <div>
+                    <img src={logo} className="header-logo" alt="logotype"/>
+                </div>
+
+                <div className="header-links-wrapper">
+                    {
+                        links.map(item => {
+                            return (
+                                <div className="nav-item" key={item.url}>
+                                    <Link to={item.url}
+                                          className="header-links-wrapper-link nav-link">{item.name}</Link>
+                                </div>
+                            );
+                        })
+                    }
+
+                </div>
+
                 <SearchField
                     handleSubmit={handleSubmit}
                     handleChange={handleChange}
                     toFoundMovies={toFoundMovies}
                 />
+
             </div>
         </div>
     );
