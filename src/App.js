a47fccfed29e25248b7221f6f0802191bd243362
PashaShevchuk <pasha608@gmail.com>
2020-07-27 20:29:23 +0300

    - add responsive styles

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import {Provider} from 'react-redux';
 import {moviesStore} from "./store";
 import MainPage from "./MainPage";
 
-import './App.css';
-
 export function App() {
     return (
         <Provider store={moviesStore}>
--- src/components/panel/Panel.js
+++ src/components/panel/Panel.js
@@ -18,7 +18,7 @@ export class Panel extends Component {
         const {isOpen} = this.state;
 
         return (
-            <div className="may-panel card " onClick={this.onOpenAndClosePanelClick}>
+            <div className="may-panel card" onClick={this.onOpenAndClosePanelClick}>
                 <div className="may-panel-header card-header">
                     <div><b>{label}</b></div>
                     <div className={`may-panel-header-chevron ${isOpen ? 'up' : ''}`}>
